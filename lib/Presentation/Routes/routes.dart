1720c58c7666409a4826b9d5120e04d7edf44d97
Daniel Sanabria <sanabriabrasil@hotmail.com>
2023-04-15 02:04:10 -0400

    DEV - update routes

--- lib/Presentation/Routes/routes.dart
+++ lib/Presentation/Routes/routes.dart
@@ -1,15 +1,35 @@
-
-import 'package:poc_flutter_empty/Presentation/Pages/Login/login_viewpagemodel.dart';
-
+import '../../App/auth_service.dart';
+import '../Pages/Home/home_viewpagemodel.dart';
 import '../Pages/Login/login_viewpage.dart';
+import '../Pages/Home/home_viewpage.dart';
+import '../../Presentation/DependencyInjection/getit.dart';
 import 'package:go_router/go_router.dart';
 
+import '../Views/Home/home_viewmodel.dart';
+
 final routes = GoRouter(
-    initialLocation: '/login',
+    initialLocation: '/',
+    refreshListenable: getIt<AuthService>(),
+    redirect: (context, state) {
+      final isAuthenticated = getIt<AuthService>().isAuthenticated;
+      final isLoginRoute = state.subloc == '/login';
+
+      if (!isAuthenticated) {
+        return isLoginRoute ? null : '/login';
+      }
+
+      if (isLoginRoute) return '/';
+      return null;
+    },
     routes: [
       GoRoute(
           path: '/login',
           builder: (context, state) {
-            return LoginViewPage(context);
-          })
+            return getIt.get<LoginViewPage>();
+          }),
+      GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return getIt.get<HomeViewPage>();
+          }),
     ]);
