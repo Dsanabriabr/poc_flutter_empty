da895bae41f8069046268858b0bfec845cf48170
Daniel Sanabria <sanabriabrasil@hotmail.com>
2023-04-15 06:00:57 -0400

    DEV - add network_repository

---
+++ lib/Data/Repositories/network_repository.dart
@@ -0,0 +1,22 @@
+import '../../App/constants.dart';
+import '../../Data/Providers/network_provider.dart';
+import '../../Domain/DTOs/request_data.dart';
+import '../../Domain/Interfaces/Repositories/network_repository_interdace.dart';
+import 'package:result_dart/result_dart.dart';
+
+//typedef AsyncResult<S, F> = Future<Result<Object, Object>>;
+
+class NetworkRepository implements NetworkRepositoryInterface {
+  NetworkProvider provider;
+  NetworkRepository(this.provider);
+
+  @override
+  AsyncResult<Object, Exception> getImageData(RequestData data) {
+    return provider.request(HTTPMethods.post, endpoint, data.convertMap());
+  }
+
+  @override
+  AsyncResult<Object, Exception> getStoryData(RequestData data) {
+    return provider.request(HTTPMethods.post, endpoint, data.convertMap());
+  }
+}
