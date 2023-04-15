babc6fde6c43a4c47c00af4aa3118eecea53e576
Daniel Sanabria <sanabriabrasil@hotmail.com>
2023-04-15 02:03:56 -0400

    DEV - update loginviewpage

--- lib/Presentation/Pages/Login/login_viewpage.dart
+++ lib/Presentation/Pages/Login/login_viewpage.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import '../../../App/auth_service.dart';
+import '../../../Presentation/DependencyInjection/getit.dart';
 
-class LoginViewPage extends StatelessWidget {
-  const LoginViewPage(BuildContext context, {Key? key}) : super(key: key);
+class LoginViewPage extends StatefulWidget {
+  const LoginViewPage({Key? key}) : super(key: key);
+ 
+ @override
+  State<LoginViewPage> createState() => _LoginViewPageState();
+}
+
+class _LoginViewPageState extends State<LoginViewPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
             child: OutlinedButton(
-      onPressed: () {  },
+      onPressed: () => getIt<AuthService>().login(),
       child: const Text('Entrar no App'),
     )));
   }
