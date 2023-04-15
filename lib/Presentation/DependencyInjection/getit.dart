56ac6b5f285211a57c9e530ba32c95d0cc8109ae
Daniel Sanabria <sanabriabrasil@hotmail.com>
2023-04-15 06:05:10 -0400

    DEV - update dependency injection useCase + repository

--- lib/Presentation/DependencyInjection/getit.dart
+++ lib/Presentation/DependencyInjection/getit.dart
@@ -1,4 +1,10 @@
+import 'package:poc_flutter_empty/Domain/Interfaces/UseCases/get_story_usecase_interface.dart';
+
 import '../../App/auth_service.dart';
+import '../../Data/Providers/network_provider.dart';
+import '../../Data/Repositories/network_repository.dart';
+import '../../Domain/Interfaces/Repositories/network_repository_interface.dart';
+import '../../Domain/UseCases/get_story_usecase.dart';
 import '../../Presentation/Pages/Home/home_viewpage.dart';
 import '../../Presentation/Pages/Home/home_viewpagemodel.dart';
 import '../../Presentation/Pages/Login/login_viewpage.dart';
@@ -13,7 +19,12 @@ void setupGetIt() {
   getIt.registerFactory<LoginViewPage>(() => const LoginViewPage());
   getIt.registerFactory<HomeViewModel>(() => HomeViewModel());
   getIt.registerFactory<HomeViewPageModel>(() => HomeViewPageModel(
-      getIt.get<HomeViewModel>()));
+      getIt.get<HomeViewModel>(), getIt.get<GetStoryUseCaseInterface>()));
   getIt.registerFactory<HomeViewPage>(
       () => HomeViewPage(viewModel: getIt.get<HomeViewPageModel>()));
+
+  getIt.registerFactory<NetworkProvider>(() => NetworkProvider());
+  getIt.registerFactory<NetworkRepositoryInterface>(() => NetworkRepository(getIt.get<NetworkProvider>()));
+  getIt.registerFactory<GetStoryUseCaseInterface>(() => GetStoryUseCase(getIt.get<NetworkRepositoryInterface>()));
+
 }
