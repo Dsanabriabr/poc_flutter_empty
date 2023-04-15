1997fe6424875d2ef6a0e633cdd4ea357e9da4ca
Daniel Sanabria <sanabriabrasil@hotmail.com>
2023-04-15 06:04:32 -0400

    FIX - Update endpoint

--- lib/App/constants.dart
+++ lib/App/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 const String appTitle = "Dream Weavers";
+const String endpoint = "http://localhost:3000/";
 const MaterialColor primaryColor = Colors.green;
 const double defaultPadding = 16.0;
